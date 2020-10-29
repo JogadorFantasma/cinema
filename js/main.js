40d8bea6d1f5a9295106bf3f9032476d3bcb59e0
bacmarcio <bac.marcio@gmail.com>
2020-10-29 18:07:36 -0300

    Pagina de Contato

--- js/main.js
+++ js/main.js
@@ -115,4 +115,13 @@ $(window).on('load', function() {
   });
 })
 
+function toggleIcon(e) {
+  $(e.target)
+      .prev('.panel-heading')
+      .find(".more-less")
+      .toggleClass('fa-plus fa-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
+
 })(jQuery); 
