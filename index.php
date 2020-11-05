9e5d173d662b16aa3c29fd5121dc979c41134a7c
Adriano Monteiro <asmbmonteiro@gmail.com>
2020-11-05 08:49:48 -0300

    Ajustes

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ $dadosCidades = $cidades->rsDadosCidades();
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                     <div class="topbar-left">
 
-                        <select class="selectpicker" onChange="AtualizaJanela('inc-horario-filmes.php?data='+this.value, 'Filmes');">
+                        <select class="selectpicker" onChange="AtualizaJanela('<?php echo SITE_URL;?>/inc-horario-filmes.php?data='+this.value, 'Filmes');">
                             <?php 
                             $datas = render(date('Y-m-d'));
                             foreach($datas as $data){ ?>
